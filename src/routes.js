56614a9ea53548422f66adf0bfcd9428be9f71d4
barretoMarcosPaulo <marcosbarretopaulo@gmail.com>
2020-04-20 02:54:08 -0300

    rota de mudar email

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import {createAppContainer , createSwitchNavigator} from 'react-navigation'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Home from './pages/Home'
+import EmailChange from './pages/EmailChange'
 
 const Routes = createAppContainer(
     createSwitchNavigator({
         Login,
         Register,
-        Home
+        Home,
+        EmailChange
     })
 )
 
